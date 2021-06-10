7b0ac85f60df498ebb1143bb4bb2c73da15065b6
Wiktor Kania <roller47@wp.pl>
2021-06-10 22:26:23 +0200

    refactored tooltips code

--- scripts/javascript/tooltip.js
+++ scripts/javascript/tooltip.js
@@ -1,26 +1,41 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.addToolTip = exports.removeTooltips = exports.createTooltip = void 0;
 const utils_1 = require("./utils");
-(async () => {
+function createTooltip(text, pos) {
+    const tooltip = utils_1.create('div', { class: 'tooltip' }, text);
+    tooltip.style.top = `${pos.top}px`;
+    tooltip.style.left = `${pos.left + 10}px`;
+    document.body.append(tooltip);
+    setTimeout(() => tooltip.classList.add('visible'), 20);
+}
+exports.createTooltip = createTooltip;
+function removeTooltips() {
+    // for now this function assumes that only one tooltip can exists at a time
+    // in case that changes this function needs to be rewritten
+    utils_1.getAll('.tooltip').forEach(tooltip => {
+        tooltip.classList.remove('visible');
+        tooltip.addEventListener('transitionend', e => tooltip.remove());
+    });
+}
+exports.removeTooltips = removeTooltips;
+function addToolTip(element, text) {
+    // for dynamically created tooltips
+    element.addEventListener('mouseenter', function (e) {
+        createTooltip(text, utils_1.getElementOffset(this));
+    });
+    element.addEventListener('mouseleave', removeTooltips);
+}
+exports.addToolTip = addToolTip;
+(() => {
     utils_1.getAll('[tooltip]').forEach(element => {
         element.addEventListener('mouseenter', function (e) {
             const tipStr = this.getAttribute('tooltip');
             if (!tipStr)
                 return;
             const pos = utils_1.getElementOffset(this);
-            const tooltip = utils_1.create('div', { class: 'tooltip' }, tipStr);
-            tooltip.style.top = `${pos.top}px`;
-            tooltip.style.left = `${pos.left + 10}px`;
-            document.body.append(tooltip);
-            setTimeout(() => tooltip.classList.add('visible'), 20);
+            createTooltip(tipStr, pos);
         });
-        element.addEventListener('mouseleave', function (e) {
-            // for now this function assumes that only one tooltip can exists at a time
-            // in case that changes this function needs to be rewritten
-            utils_1.getAll('.tooltip').forEach(tooltip => {
-                tooltip.classList.remove('visible');
-                tooltip.addEventListener('transitionend', e => tooltip.remove());
-            });
-        });
+        element.addEventListener('mouseleave', removeTooltips);
     });
 })();
--- scripts/typescript/tooltip.ts
+++ scripts/typescript/tooltip.ts
@@ -1,25 +1,43 @@
-import { Global } from "./global";
-import { create, get, getAll, getElementOffset } from "./utils";
+import { create, getAll, getElementOffset } from "./utils";
 
-(async () => {
+export interface TooltipPosition {
+  top: number
+  left: number
+}
+
+export function createTooltip(text: string, pos: TooltipPosition): void {
+  const tooltip = create('div', {class: 'tooltip'}, text);
+  tooltip.style.top = `${pos.top}px`;
+  tooltip.style.left = `${pos.left + 10}px`;
+  document.body.append(tooltip);
+  setTimeout(() => tooltip.classList.add('visible'), 20)
+}
+
+export function removeTooltips(): void {
+  // for now this function assumes that only one tooltip can exists at a time
+  // in case that changes this function needs to be rewritten
+  getAll('.tooltip').forEach(tooltip => {
+    tooltip.classList.remove('visible');
+    tooltip.addEventListener('transitionend', e => tooltip.remove());
+  });
+}
+
+export function addToolTip(element: HTMLElement, text: string) {
+  // for dynamically created tooltips
+  element.addEventListener('mouseenter', function(e) {
+    createTooltip(text, getElementOffset(this));
+  });
+  element.addEventListener('mouseleave', removeTooltips);
+}
+
+(() => {
   getAll('[tooltip]').forEach(element => {
     element.addEventListener('mouseenter', function(e) {
       const tipStr = this.getAttribute('tooltip');
       if (!tipStr) return;
       const pos = getElementOffset(this);
-      const tooltip = create('div', {class: 'tooltip'}, tipStr);
-      tooltip.style.top = `${pos.top}px`;
-      tooltip.style.left = `${pos.left + 10}px`;
-      document.body.append(tooltip);
-      setTimeout(() => tooltip.classList.add('visible'), 20)
+      createTooltip(tipStr, pos);
     });
-    element.addEventListener('mouseleave', function(e) {
-      // for now this function assumes that only one tooltip can exists at a time
-      // in case that changes this function needs to be rewritten
-      getAll('.tooltip').forEach(tooltip => {
-        tooltip.classList.remove('visible');
-        tooltip.addEventListener('transitionend', e => tooltip.remove());
-      });
-    });
+    element.addEventListener('mouseleave', removeTooltips);
   });
 })();
