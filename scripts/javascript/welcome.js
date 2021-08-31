c9d6265577dc7ea661a8c0245e3d3b67324af7f8
Wiktor Kania <roller47@wp.pl>
2021-08-31 19:45:19 +0200

    add dummy even listeners

--- scripts/javascript/welcome.js
+++ scripts/javascript/welcome.js
@@ -1,8 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.showWelcomeScreen = void 0;
+exports.hideWelcomeScreen = exports.showWelcomeScreen = void 0;
 const utils_1 = require("./utils");
 async function showWelcomeScreen() {
     return await utils_1.fadeIn(utils_1.get('.welcome-container'), 400);
 }
 exports.showWelcomeScreen = showWelcomeScreen;
+async function hideWelcomeScreen() {
+    return await utils_1.fadeOut(utils_1.get('.welcome-container'), 400);
+}
+exports.hideWelcomeScreen = hideWelcomeScreen;
+utils_1.get('#welcome-discover').addEventListener('click', function (e) {
+    // TODO
+    hideWelcomeScreen();
+});
+utils_1.get('#welcome-create').addEventListener('click', function (e) {
+    // TODO
+    hideWelcomeScreen();
+});
+utils_1.get('#welcome-invite').addEventListener('click', function (e) {
+    // TODO
+    hideWelcomeScreen();
+});
+utils_1.get('#welcome-add-friends').addEventListener('click', function (e) {
+    // TODO
+    hideWelcomeScreen();
+});
+utils_1.get('#welcome-get-help').addEventListener('click', function (e) {
+    // TODO
+    hideWelcomeScreen();
+});
--- scripts/typescript/welcome.ts
+++ scripts/typescript/welcome.ts
@@ -1,5 +1,34 @@
-import { fadeIn, get } from "./utils";
+import { fadeIn, fadeOut, get } from "./utils";
 
 export async function showWelcomeScreen() {
   return await fadeIn(get('.welcome-container')!, 400)
 }
+
+export async function hideWelcomeScreen() {
+  return await fadeOut(get('.welcome-container')!, 400) 
+}
+
+get('#welcome-discover')!.addEventListener('click', function(this: HTMLElement, e: Event) {
+  // TODO
+  hideWelcomeScreen();
+});
+
+get('#welcome-create')!.addEventListener('click', function(this: HTMLElement, e: Event) {
+  // TODO
+  hideWelcomeScreen();
+});
+
+get('#welcome-invite')!.addEventListener('click', function(this: HTMLElement, e: Event) {
+  // TODO
+  hideWelcomeScreen();
+});
+
+get('#welcome-add-friends')!.addEventListener('click', function(this: HTMLElement, e: Event) {
+  // TODO
+  hideWelcomeScreen();
+});
+
+get('#welcome-get-help')!.addEventListener('click', function(this: HTMLElement, e: Event) {
+  // TODO
+  hideWelcomeScreen();
+});
